58f9b3b67c1241d770fad130ae8fb96db05fdda4
nowgnas <marshmello.sw@gmail.com>
2022-07-01 01:51:37 +0900

    init

---
+++ src/test1/test.java
@@ -0,0 +1,14 @@
+package test1;
+
+import java.util.Iterator;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello");
+		int a = 1;
+
+	}
+
+}
